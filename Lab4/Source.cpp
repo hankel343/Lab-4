3cd32c13c2a5542176fd556a14783af3fb51cba5
hankel343 <57358008+hankel343@users.noreply.github.com>
2020-10-14 14:11:50 -0500

    Refactored code to be able to handle variable temperatures

--- Lab4/Source.cpp
+++ Lab4/Source.cpp
@@ -1,85 +1,55 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <iomanip>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-//Function protoype:
-string StarSort(int temp);
-
-ifstream inFile;
-string lineNeg30, lineNeg20, line0, line1, line2, line3, line4, line5, line10, line50, line100;
-
 int main()
 {
-	
-	int temp;
-	string stars;
-	
-	inFile.open("test.txt");
-	inFile >> temp;	//Reads in integer values stored in "test.txt"
+	ifstream inFile;
+	float temp;
+	vector <float> data{};
 
-	while (inFile)
-	{
-		for (int i = 0; i < 1; ++i)
-		{
-			if (temp >= -30 && temp < 0)
-			{
-				lineNeg20.append("*");
-			}
-			else if (temp == 0)
-			{
-				line0.append("*");
-			}
-			else if (temp == 1)
-			{
-				line1.append("*");
-			}
-			else if (temp == 2)
-			{
-				line2.append("*");
-			}
-			else if (temp == 3)
-			{
-				line3.append("*");
-			}
-			else if (temp == 4)
-			{
-				line4.append("*");
-			}
-			else if (temp == 5)
-			{
-				line5.append("*");
-			}
-			else if (temp >= 5 && temp < 10)
-			{
-				line10.append("*");
-			}
-			else if (temp >= 10 && temp < 50)
-			{
-				line50.append("*");
-			}
-			else if (temp >= 50 && temp <= 100)
-			{
-				line100.append("*");
-			}
-		}
-		inFile >> temp; //Reads next integer value in test.txt
+	inFile.open("test.txt");
+
+	if (!inFile) {
+		cout << "Input file failed to open.";
+		return 1;
 	}
 
-	cout << "Temperatures for 24 hours:\n";
-	cout << setw(6) << "-30" << setw(12) << "0" << setw(12) << "30" << setw(12) << "60" << setw(12) << "90" << setw(12) << "120\n";
-	cout << "-20    " << setw(11) << lineNeg20 + "|" << endl;
-	cout << "0      " << setw(11) << "|" << line0 << endl;
-	cout << "1      " << setw(11) << "|" << line1 << endl;
-	cout << "2      " << setw(11) << "|" << line2 << endl;
-	cout << "3      " << setw(11) << "|" << line3 << endl;
-	cout << "4      " << setw(11) << "|" << line4 << endl;
-	cout << "5      " << setw(11) << "|" << line5 << endl;
-	cout << "10     " << setw(11) << "|" << line10 << endl;
-	cout << "50     " << setw(11) << "|" << line50 << endl;
-	cout << "100    " << setw(11) << "|" << line100 << endl;
+	inFile >> temp;
+
+	cout << setw(5) << "-30" << setw(7) << "0" << setw(10) << "30" << setw(10) << "60" << setw(10) << "90" << setw(10) << "120" << endl;
+
+	while (inFile) {
+		data.push_back(temp);
+		inFile >> temp;
+	}
+
+	for (float val : data) {
+		string starOutput;
+		starOutput = "";
+		cout << val << ":";
+
+		for (int i = 1; i <= (abs(val) / 3.0); ++i) { //Temperatures are converted to their absolute value, then divided by three for each star to represent 3 degrees.
+			starOutput.append("*");
+		}
+		if (val > -10 && val < 0) {
+			cout << setw(9) << starOutput + "|";
+		}
+		else if (val < 10) {
+			cout << setw(10) << "|" << starOutput;
+		}
+		else if (val >= 10 && val < 100) {
+			cout << setw(9) << "|" << starOutput;
+		}
+		else if (val >= 100) {
+			cout << setw(8) << "|" << starOutput;
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
