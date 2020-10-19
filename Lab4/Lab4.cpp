3353c820c4c1ef66fc625b4caf3b14524a754256
hankel343 <57358008+hankel343@users.noreply.github.com>
2020-10-19 18:15:13 -0500

    1.) Removed vector 2.) Added basic error checking 3.) Organized code into functions

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -6,120 +6,149 @@
 ********************************************************************************************************/
 
 #include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <vector>
-/*****************************************************************************************************************************
-*I choose to use a vector to store temperatures because I could have the data set of all temperatures in one location and then 
-*iterate through the vector's elements using a range based for loop.
-******************************************************************************************************************************/
+#include <fstream> //File I/O
+#include <string> //string used to store stars
+#include <iomanip> //Formating graph
+#include <cctype> //Used for error checking
 
 using namespace std;
 
-void Print_Star(vector<float>& data); //Print_Star uses a reference parameter to pass the value of the vector "data" to the Print_Star function.
+//Function Prototypes.
+
+int ErrorCheck(ifstream& inFile);
+//Precondition: File name that user types in able to be found.
+//Postcondition: All data in the input file is error free (correct data type) and can be used to print the graph.
+
+void PrintGraph(ifstream& inFile, string fileName);
+//Precondition: Input file has appropriate data.
+//Postcondition: Print bar graph representing temperatures over 24 hours.
 
 int main()
 {
 	ifstream inFile;
 	string fileName;
-	float temp;
-	vector <float> data{};
 
-	cout << "Enter the name of your input file: ";
-	cin >> fileName;
-
-	inFile.open(fileName.c_str());
-
-	//In the following block the user either enters a valid file name or decides to terminate program in this loop when the input stream is in the failed state.
-	if (!inFile) {
-		do
-		{
-			cout << "The input file failed to open.\n";
-			cout << "Try again to enter the file name or alternatively enter 'Q' or 'q' to terminate this program.\n\n";
-			cout << "File name:";
-			cin >> fileName;
-
-			if (fileName == "Q" || fileName == "q") { //User decides to terminate program.
-				cout << "Goodbye.";
-				return 1; //Terminates program.
-			}
-
-			inFile.open(fileName.c_str()); //Attempts to open file stream again
-		} while (!inFile); 
-	}
-
-	cout << "\n\nOpening file..."; //Program successfully finds input file.
-
-	inFile >> temp;
-
-	//EOF controlled while loop that reads temperatures in from input file and adds them to a vector
-	while (inFile)
+	//Following do-while prompts user for filename.
+	do
 	{
-		data.push_back(temp);
-		inFile >> temp;
+		cout << "Enter the name of your input file: ";
+		cin >> fileName;
+		inFile.open(fileName.c_str());
+
+		if (!inFile)
+		{
+			cout << "Input file failed to open. Make sure you typed its name correctly and that the input file exists.\n";
+			cout << "Try again or press 'Q' to quit: ";
+			cin >> fileName;
+			cout << "\n\n\n";
+
+			if (fileName == "Q" || fileName == "q") //User terminates program. 
+			{
+				cout << "Goodbye...\n\n\n";
+				return 1; //User terminates program.
+			}
+			inFile.open(fileName.c_str());
+		}
+	} while (!inFile);
+
+
+	/*********************************************************************************************************
+	*ErrorCheck is a value returning function of type int.
+	*It returns 1 if an error is present, so the if statement terminates the program if the call is equal to 1
+	**********************************************************************************************************/
+	if (ErrorCheck(inFile) == 1)
+	{
+		return 1;
 	}
 
-	//Heading and x-axis of bar graph.
-	cout << "\n\n\nTemperatures for 24 hours: \n";
-	cout << setw(7) << "-30" << setw(8) << "0" << setw(10) << "30" << setw(10) << "60" << setw(10) << "90" << setw(10) << "120" << endl;
+	//Print graph reads data in from the input file, determines how many stars are needed, and then prints the graph to the screen.
+	PrintGraph(inFile, fileName);
 
-	//Prints y-axis of hourly temperatures and a corresponding number of stars.
-	Print_Star(data);
-	//Precondition: All input from the input file is either an int or floating type value
-	//Postcondition: A graph of hourly temperatures is printed.
-
-	cout << "\n#############\n";
-	cout << "* = 3 degrees";
-	cout << "\n#############";
-
-
-	inFile.close();
 	return 0;
 }
 
-void Print_Star(vector<float>& data)
+int ErrorCheck(ifstream& inFile)
 {
-	//Range based for loop that loops through each item in the vector named "data" that stored temps from input file. Values in "data" are also used to calculate star output.
-	for (auto val : data) {
+	char errorCheck;
+	//Priming read for vetting values in input file
+	inFile >> errorCheck;
 
-		string starOutput; //Stores stars for output to the console.
-		starOutput = ""; //Resets the string that stars are stored in every iteration through the ranged based for loop on the vector "data".
+	//Following while loop reads does an initial read through the input file to determine if any invalid data types are present.
+	while (inFile)
+	{
+		if (isalpha(errorCheck)) //True when input character is a letter.
+		{
+			cout << "\n\n\n###ERROR###\n";
+			cout << "*********************************************************************************************\n";
+			cout << "The input stream has encountered an invalid input type " << "'" << errorCheck << "'" << " in the input stream.\n";
+			cout << "Ensure that the input file only contains data of type integer or float.\n";
+			cout << "*********************************************************************************************\n";
+			return 1; //Errors were found in the input file and program terminates.
+		}
+		inFile >> errorCheck;
+	}
+	inFile.close();
+	return 0; //Means no errors where found in the input file.
+}
 
-		//Following if statements vet out any values out of range -30 to 120 so they don't appear on the graph.
-		if (val < -30 || val > 120) {
+void PrintGraph(ifstream& inFile, string fileName)
+{
+	float temp;
+
+	cout << "\nOpening file...";
+
+
+	//Heading for graph.
+	cout << "\n\nTemperatures for 24 hours: \n";
+	cout << setw(8) << "-30" << setw(8) << "0" << setw(10) << "30" << setw(10) << "60" << setw(10) << "90" << setw(10) << "120" << endl;
+
+	inFile.open(fileName.c_str());
+	inFile >> temp;
+
+	while (inFile)
+	{
+		temp = ceil(temp); //Ceil is used to round any floating point values to a whole number. Avoids graph being off due to decimal char.
+
+		string starOutput; //Stores stars
+		starOutput = ""; //Resets the contents of the string every iteration.
+
+		//The following if else block functions as a way to vet out of bound temperatures from the input file.
+		if (temp < -30 || temp > 120) { //If temp is too high or low, a new value is read in from the input file and then iteration of the outer while loop begins again.
+			inFile >> temp;
 			continue; //Tells compiler to skip body of loop and return to loop condition of first for loop.
-		} else {
-			cout << val << ":";
+
+		}
+		else { //temp in range
+			cout << temp << ":";
 		}
 
-		//This for loop takes the temperature read from the input file and divides it by three to get the correct number of degrees per star (1 star = 3 deg).
-		for (int i = 1; i <= (abs(val) / 3.0); ++i) { //Temperatures are converted to their absolute value, then divided by three for each star to represent 3 degrees.
-			starOutput.append("*"); //For every iteration that i is less than or equal to the temp divided by three, a star is added to the string starOutput.
+		//This for loop is used to get the correct amount of stars per degree.
+		for (int i = 1; i <= (abs(temp) / 3.0); ++i) //abs() is used to calculate values for negative inputs.
+		{
+			starOutput.append("*");
 		}
 
 		//Following if else ladder prints the corressponding temperature and number of stars associated with that value
-		if (val == -30) {
-			cout << setw(10) << starOutput + "|";
-		}
-		else if (val > -30 && val <= -10) {
-			cout << setw(11) << starOutput + "|";
-		}
-		else if (val > -10 && val < 0) {
+		if (temp == -30) {
 			cout << setw(12) << starOutput + "|";
 		}
-		else if (val >= 0 && val < 10) {
+		else if (temp > -30 && temp <= -10) {
+			cout << setw(12) << starOutput + "|";
+		}
+		else if (temp > -10 && temp < 0) {
+			cout << setw(13) << starOutput + "|";
+		}
+		else if (temp >= 0 && temp < 10) {
+			cout << setw(14) << "|" << starOutput;
+		}
+		else if (temp >= 10 && temp < 100) {
 			cout << setw(13) << "|" << starOutput;
 		}
-		else if (val >= 10 && val < 100) {
+		else if (temp >= 100 && temp <= 120) {
 			cout << setw(12) << "|" << starOutput;
 		}
-		else if (val >= 100 && val <= 120) {
-			cout << setw(11) << "|" << starOutput;
-		}
-		else if (val > 120) {
-			continue;
-		}
-		
+
 		cout << endl;
+		inFile >> temp;
 	}
 }
